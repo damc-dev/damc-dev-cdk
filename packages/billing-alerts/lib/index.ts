555e502ae0e741426704c23dc9a51f38814df893
David McElligott <damc.dev@gmail.com>
2020-06-02 00:43:01 -0500

    style: 💄 Added eslint-tslint

--- packages/billing-alerts/lib/index.ts
+++ packages/billing-alerts/lib/index.ts
@@ -6,58 +6,61 @@ import * as subscriptions from '@aws-cdk/aws-sns-subscriptions';
 import { Alarm, Metric, ComparisonOperator } from '@aws-cdk/aws-cloudwatch';
 
 export interface BillingAlertProps {
-    /**
-     * The threshold of estimated charges in dollars to alert on
-     * @default = 1
-     */
-    readonly alarmThreshold?: number;
+  /**
+   * The threshold of estimated charges in dollars to alert on
+   * @default = 1
+   */
+  readonly alarmThreshold?: number;
 
-    /**
-     * Email addresses to send alerts to
-     * @default = []
-     */
-    readonly emailSubscriptions?: string[];
+  /**
+   * Email addresses to send alerts to
+   * @default = []
+   */
+  readonly emailSubscriptions?: Array<string>;
 }
 
 export class BillingAlert extends cdk.Construct {
-    /**
-     * SNS topic alerts get published to
-     */
-    public readonly topic: sns.Topic;
+  /**
+   * SNS topic alerts get published to
+   */
+  public readonly topic: sns.Topic;
 
-    /**
-     * Billing alarm
-     */
-    public readonly alarm: Alarm;
+  /**
+   * Billing alarm
+   */
+  public readonly alarm: Alarm;
 
-    constructor(scope: cdk.Construct, id: string, props: BillingAlertProps = {}) {
-        super(scope, id);
+  constructor(scope: cdk.Construct, id: string, props: BillingAlertProps = {}) {
+    super(scope, id);
 
-        this.topic = new sns.Topic(this, 'BillingTopic');
+    this.topic = new sns.Topic(this, 'BillingTopic');
 
-        const estimatedCharges = new Metric({
-            namespace: 'AWS/Billing',
-            metricName: 'EstimatedCharges',
-            dimensions: {
-                Currency: 'USD'
-            }
-        });
+    const estimatedCharges = new Metric({
+      namespace: 'AWS/Billing',
+      metricName: 'EstimatedCharges',
+      dimensions: {
+        Currency: 'USD',
+      },
+    });
 
-        this.alarm = new Alarm(this, 'BillingAlarm', {
-            alarmDescription: 'Alert when estimated charges exceed threshold',
-            metric: estimatedCharges,
-            threshold: props.alarmThreshold || 1,
-            comparisonOperator: ComparisonOperator.GREATER_THAN_THRESHOLD,
-            evaluationPeriods: 1,
-            statistic: 'max'
-        });
+    this.alarm = new Alarm(this, 'BillingAlarm', {
+      alarmDescription: 'Alert when estimated charges exceed threshold',
+      metric: estimatedCharges,
+      threshold: props.alarmThreshold || 1,
+      comparisonOperator: ComparisonOperator.GREATER_THAN_THRESHOLD,
+      evaluationPeriods: 1,
+      statistic: 'max',
+    });
 
-        this.alarm.addAlarmAction(new cw_actions.SnsAction(this.topic));
+    this.alarm.addAlarmAction(new cw_actions.SnsAction(this.topic));
 
-        if(props.emailSubscriptions !== undefined && props.emailSubscriptions.length > 0) {
-            props.emailSubscriptions.forEach((email) => {
-                this.topic.addSubscription(new subscriptions.EmailSubscription(email));
-            });
-        }
+    if (
+      props.emailSubscriptions !== undefined &&
+      props.emailSubscriptions.length > 0
+    ) {
+      props.emailSubscriptions.forEach((email) => {
+        this.topic.addSubscription(new subscriptions.EmailSubscription(email));
+      });
     }
+  }
 }
